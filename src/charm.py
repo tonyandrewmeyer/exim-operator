a78b62705018ecae7e0cbf84eca1d22b309553f2
Tony Meyer <tony.meyer@canonical.com>
2023-09-12 14:40:34 +1200

    Add basic plan.
    Add an initial Pebble plan for Exim, and use that to set the workload version (from Exim).

--- src/charm.py
+++ src/charm.py
@@ -5,6 +5,7 @@
 """Charm the application."""
 
 import logging
+import re
 
 import ops
 
@@ -16,11 +17,66 @@ class EximCharm(ops.CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
+        self.pebble_service_name = "exim-service"
+        self.container = self.unit.get_container("exim")
         self.framework.observe(self.on["exim"].pebble_ready, self._on_pebble_ready)
 
     def _on_pebble_ready(self, event: ops.PebbleReadyEvent):
         """Handle pebble-ready event."""
+        container = event.workload
+        # Add initial Pebble config layer using the Pebble API
+        container.add_layer("exim", self._pebble_layer, combine=True)
+        # Make Pebble reevaluate its plan, ensuring any services are started if enabled.
+        container.replan()
+        self.unit.set_workload_version(self.version)
         self.unit.status = ops.ActiveStatus()
+
+    @property
+    def _pebble_layer(self) -> ops.pebble.Layer:
+        """Return a dictionary representing a Pebble layer."""
+        command = " ".join(
+            [
+                "/usr/sbin/exim",
+                "-bd",
+                "-q 30m",
+            ]
+        )
+        pebble_layer: ops.pebble.LayerDict = {
+            "summary": "Exim MTA service",
+            "description": "pebble config layer for Exim MTA server",
+            "services": {
+                self.pebble_service_name: {
+                    "override": "replace",
+                    "summary": "exim MTA",
+                    "command": command,
+                    "startup": "enabled",
+                }
+            },
+        }
+        return ops.pebble.Layer(pebble_layer)
+
+    @property
+    def version(self) -> str:
+        """Reports the current workload (Exim) version."""
+        if self.container.can_connect() and self.container.get_services(self.pebble_service_name):
+            try:
+                return self._request_version()
+            # Catching Exception is not ideal, but we don't care much for the error here, and just
+            # default to setting a blank version since there isn't much the admin can do!
+            except Exception as e:
+                logger.warning("unable to get version from Exim: %s", str(e))
+                logger.exception(e)
+        return ""
+
+    def _request_version(self) -> str:
+        """Fetch the version from the running workload using a subprocess."""
+        # XXX This could handle errors (e.g. if Exim is missing) more gracefully.
+        exim = self.container.exec(["/usr/sbin/exim", "-bV"])
+        out = exim.wait_output()[0]
+        mo = re.search(r".+(\d+\.\d+ #\d+).+", out)
+        # `juju status` won't show a version that has a space or a '#' (or '+'),
+        # so adjust it slightly.
+        return mo.groups()[0].replace(" #", "-debian-") if mo else "unknown"
 
 
 if __name__ == "__main__":  # pragma: nocover
